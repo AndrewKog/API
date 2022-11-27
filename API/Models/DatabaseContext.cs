70a9e44aef99a1e83f0347c024b0f153cf8b3a52
ΑΝΔΡΕΑΣ ΚΟΓΙΑΣ <kogias22@gmail.com>
2022-11-27 15:41:01 +0200

    Add project files.

---
+++ API/Models/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Models
+{
+ 
+public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Match> SelectItems { get; set; } = null!;
+    }
+    }
---
+++ API/Models/Match.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+ 
+
+public class Match
+    {
+        public long Id { get; set; }
+        public string? Description { get; set; }
+
+        public DateTime MatchDate { get; set; }
+
+        public DateTime MatchTime { get; set; }
+        public string? TeamA { get; set; }
+        public string? TeamB { get; set; }
+
+    }
+
+    }
